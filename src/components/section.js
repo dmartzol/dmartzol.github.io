452f850af851cfc745cd0b9f0e84136ecf039ce3
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-09-10 01:53:49 -0500

    adding background color to sections and page title

--- src/components/section.js
+++ src/components/section.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import sectionStyles from './section.module.css'
+
 
 export default function Section({ title, children }) {
     return (
-        <section>
-            <h2>{title}</h2>
+        <section className={sectionStyles.section}>
+            <h2 className={sectionStyles.title}>{title}</h2>
             {children}
         </section>
     )
